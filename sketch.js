2d13a0dff86a6de52ad6cc69928199760d045d71
joeljoseidicula <74285018+joeljoseidicula@users.noreply.github.com>
2020-11-16 18:05:44 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,14 +5,16 @@ function setup() {
   createCanvas(1600,400);
 speed=random(223,321);
 weight=random(30,52);
-thickness= random(22,83);
+thickness= random(28,32);
 
-  bullet=createSprite(50, 200, 50, 50);
-bullet.velocityX= speed;
-bullet.shapeColor=255;
+bullet= createSprite(50,200,50,10);
+bullet.velocityX=speed;
+bullet.shapeColor=color(255);
 
-  wall=createSprite(1000,200,thickness,height/2);
-  wall.shapeColor=80,80,80;
+wall= createSprite(500,200,thickness,height/2) 
+wall.shapeColor= color(80,80,80)
+  
+    
 }
 
 function draw() {
@@ -20,6 +22,7 @@ function draw() {
   if(wall.x-bullet.x<(bullet.width+wall.width)/2){
     ball.velocityX=0;
   var deformation=0.5*weight*speed*speed/22509
+  }
   if(deformation>180){
     bullet.shapeColor=color(255,0,0);
   }
@@ -28,15 +31,14 @@ function draw() {
   }
   if(deformation<100){
     bullet.shapeColor=color(0,255,0);
-  }
-  function hasCollided(buller,wall){
-bulletRightEdge=bullet.x+bullet.width;
-wallLeftEdge= wall.x;
-if (bulletRightEdge>=wallLeftEdge){
-  return true
-}
-return false;
-  }
+  }function hasCollided(buller,wall){
+    bulletRightEdge=bullet.x+bullet.width;
+    wallLeftEdge= wall.x;
   
+  if (bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false;
+    }
   drawSprites();
 }
